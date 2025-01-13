c3afec35fe460ef450d6a6f1f6ccddc9b345e0c5
Omed Akram <116754918+Omed0@users.noreply.github.com>
2025-01-14 02:51:34 +0300

    zyadkrdni kashf 7sabi qasaa

--- loader.js
+++ loader.js
@@ -5,4 +5,3 @@ export default function myImageLoader({ src, width, quality }) {
     return `http://localhost:3000/api/images/${src}?w=${width}&q=${quality || 75}`
 }
 
-//this for serve images from router images
